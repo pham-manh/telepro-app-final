abaa02fd3c60c40c1c57e4fd62b69c756ee0d15d
Manh Pham <manhk979@gmail.com>
2024-04-22 22:19:51 +0700

    [ADD] Impl CallList UI

--- src/screens/CallListScreen.jsx
+++ src/screens/CallListScreen.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
+import CallLink from '../components/CallLink';
+import RecentCalls from '../components/RecentCalls';
+import {Colors} from '../theme/Colors';
 
 const CallListScreen = () => {
   return (
-    <View>
-      <Text>CallListScreen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <CallLink />
+      <RecentCalls />
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+    padding: 16,
+  },
+});
 
 export default CallListScreen;
